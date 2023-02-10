52ee995ea962625fb3c5378cdf8281b5e22702d2
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-02-10 08:37:04 -0600

    Initial Commit

---
+++ swiftLoopsExercise.playground/Contents.swift
@@ -0,0 +1,22 @@
+
+for i in 1...7 {
+    print(i)
+}
+
+let greetings = [
+"Hello",
+"Hi",
+"Whats up"]
+
+for i in 0..<greetings.count {
+    print(greetings[i])
+}
+
+var num1 = 5
+while num1 <= 25{
+    print(num1)
+    num1 = num1 + 4
+}
+
+// question 4
+//"Hello World" will print 1 time
